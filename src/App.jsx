0a5db4971a78cd9f39f42ee12e8fa67c9bb496b2
davismiler <0xaliasrowyan@gmail.com>
2025-08-08 19:30:06 +0530

    feat: added multiple pages with data handling

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Category from './components/Category'
+import FeaturedProducts from './components/FeaturedProducts'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Category />
+      <FeaturedProducts />
     </div>
   )
 }
--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 const CategoryCard = ({ name, image }) => {
     return (
         <div className='overflow-hidden transition-all hover:shadow-md border border-gray-200 rounded-xl'>
-            <div className='aspect-square relative bg-gray-200'>
-                <img src={image} alt={name} className='object-cover w-full h-full' />
+            <div className=''>
+                <div className='aspect-square relative bg-gray-200'>
+                    <img src={image} alt={name} className='object-cover w-full h-full' />
+                </div>
                 <div className='p-4 text-center'>
                     <h3 className='font-medium'>{name}</h3>
                 </div>
