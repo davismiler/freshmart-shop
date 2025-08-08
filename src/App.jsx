e7bf9996c8fb7d550961e97e4032e3278937fa8c
davismiler <0xaliasrowyan@gmail.com>
2025-08-08 19:38:39 +0530

    feat: integrate Redux and modify affected components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Category from './components/Category'
-import FeaturedProducts from './components/FeaturedProducts'
+import Footer from './components/Footer'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Shop from './Pages/Shop'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Hero />
-      <Category />
-      <FeaturedProducts />
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/shop' element={<Shop/>}></Route>
+          <Route path='/about' element={<About/>}></Route>
+          <Route path='/contact' element={<Contact/>}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   )
 }
 
